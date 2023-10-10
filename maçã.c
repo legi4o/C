f8546bc6650772ca14de0f8bdf505931a585f841
Matheus Araújo <144128647+legi4o@users.noreply.github.com>
2023-10-10 09:53:41 -0300

    Add files via upload

---
+++ maçã.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+
+int quantidademaca;
+float total;
+
+int main()
+{
+
+    setlocale(LC_ALL, "portuguese");
+
+    printf("Digite a quantidade que deseja comprar: ");
+    scanf("%d", &quantidademaca);
+
+    if (quantidademaca >= 6)
+    {
+        total = 1.30 * quantidademaca;
+    }
+
+    if (quantidademaca >= 12)
+
+    {
+        total = 1.00 * quantidademaca;
+    }
+
+    printf("\n\n");
+    printf("Quantidade de maçãs: %d \n", quantidademaca);
+    printf("\n");
+    printf("valor total: %.2f \n", total);
+
+    return 0;
+}
