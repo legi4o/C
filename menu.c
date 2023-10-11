013e4f5c341c6efffb3cf7cc2e3a2be345960555
Matheus Araújo <144128647+legi4o@users.noreply.github.com>
2023-10-11 08:11:51 -0300

    Add files via upload

--- menu.c
+++ menu.c
@@ -3,58 +3,57 @@
 #include <string.h>
 #include <locale.h>
 
-int main() {
+int main()
+{
 
-int codigo;
+    int codigo;
 
-printf("------------------ MENU --------------------");
-printf("\n");
-printf("| codigo   |       prato        |   valor  |");
-printf("\n");
-printf("|    1     |     picanha        |   25.00  |");
-printf("\n");
-printf("|    2     |     lasanha        |   25.00  |");
-printf("\n");
-printf("|    3     |    strogonoff      |   25.00  |");
-printf("\n");
-printf("|    4     |   bife acebolado   |   25.00  |");
-printf("\n");
-printf("|    5     |    pão com ovo     |   25.00  |");
-printf("\n");
-printf("--------------------------------------------");
-printf("\n\n");
+    printf("------------------ MENU --------------------");
+    printf("\n");
+    printf("| codigo   |      prato         |   valor  |");
+    printf("\n");
+    printf("|    1     |     picanha        |   25.00  |");
+    printf("\n");
+    printf("|    2     |     lasanha        |   20.00  |");
+    printf("\n");
+    printf("|    3     |    strogonoff      |   18.00  |");
+    printf("\n");
+    printf("|    4     |   bife acebolado   |   15.00  |");
+    printf("\n");
+    printf("|    5     |    pão com ovo     |   5.00   |");
+    printf("\n");
+    printf("--------------------------------------------");
+    printf("\n\n");
 
-printf("Digite o codigo do prato: ");
-scanf("%d", &codigo);
+    printf(" Digite o codigo do prato: ");
+    scanf("%d", &codigo);
+    printf("\n");
 
-switch (codigo){
+    switch (codigo)
+    {
 
-case 1 :
-printf(" picanha, 25.00");
-break;
+    case 1:
+        printf(" picanha, 25.00");
+        break;
 
-}
+    case 2:
+        printf(" lasanha, 20,00");
+        break;
 
+    case 3:
+        printf(" strogonoff, 18,00");
+        break;
 
+    case 4:
+        printf(" bife acebolado, 15,00");
+        break;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    case 5:
+        printf(" pão com ovo, 5,00");
+        break;
+    default:
+        printf(" opção invalida");
+    }
 
     return 0;
 }
