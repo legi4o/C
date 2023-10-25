b2e7438b1a3b3c8e5b771477a60b6ac83fdc069d
Matheus Araújo <144128647+legi4o@users.noreply.github.com>
2023-10-25 09:07:13 -0300

    Update atividade laço de repetição inteiros.c

--- atividade laço de repetição inteiros.c
+++ atividade laço de repetição inteiros.c
@@ -2,29 +2,35 @@
 #include <stdlib.h>
 #include <locale.h>
 
-int main(){
+int main()
+{
 
-int numero;
+    int numero;
+    float media;
+    float soma = 0;
+    int contador;
 
-printf("Digite um numero: ");
-scanf("%d", &numero);
+    setlocale(LC_ALL, "portuguese");
 
+    do
+    {
+        printf("Digite um numero: ");
+        scanf("%d", &numero);
 
+        if (numero > 0)
 
+        {
+            soma += numero;
+            contador++;
+        }
 
+    } while (numero > 0);
 
+    media = soma / (float) contador;
 
+    printf("\n");
 
-
-
-
-
-
-
-
-
-
-
+    printf(" a media e: %.1f \n", media);
 
     return 0;
 }
